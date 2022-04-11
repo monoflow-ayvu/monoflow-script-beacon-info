5e353ee99103d09f6e01303c0582344a17a7e21e
Fernando Mumbach <fermuch@gmail.com>
2022-04-11 09:29:41 -0300

    adding basic structure for project

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import * as MonoUtils from "@fermuch/monoutils";
+import { BeaconData } from "./minew_data";
 
-// based on settingsSchema @ package.json
-type Config = Record<string, unknown> & {
-  nome: string;
+declare class BeaconScanEvent extends MonoUtils.wk.event.BaseEvent {
+  kind: 'beacon-scan-event';
+  beacons: BeaconData[];
+  getData(): {beacons: BeaconData[]};
 }
 
+// based on settingsSchema @ package.json
+type Config = Record<string, unknown> & {}
 const conf = new MonoUtils.config.Config<Config>();
 
-messages.on('onInit', function() {
-  const name = conf.get('name', 'default name');
-  platform.log(`Hello, ${name}!`);
+MonoUtils.wk.event.subscribe<BeaconScanEvent>('beacon-scan-event', function(event) {
+  platform.log(`Beacons: ${JSON.stringify(event.beacons)}`);
 });
---
+++ src/minew_data.ts
@@ -0,0 +1,82 @@
+interface FrameiBeacon {
+  type: 'ibeacon';
+  uuid: string;
+  major: number;
+  minor: number;
+  tx: number;
+}
+interface FrameUID {
+  type: 'uid';
+  instance: string;
+  namespace: string;
+}
+interface FrameAccSensor {
+  type: 'accelerometer';
+  x: number;
+  y: number;
+  z: number;
+}
+interface FrameHTSensor {
+  type: 'ht';
+  temperature: number;
+  humidity: number;
+}
+interface FrameTLM {
+  type: 'tlm';
+  temperature: number;
+  batteryVol: number;
+  secCount: number;
+  advCount: number;
+}
+interface FrameURL {
+  type: 'url';
+  tx: number;
+  url: string;
+}
+interface FrameLightSensor {
+  type: 'light';
+  battery: number;
+  lux: number;
+}
+interface FrameForceSensor {
+  type: 'force';
+  battery: number;
+  force: number;
+}
+interface FramePIRSensor {
+  type: 'pir';
+  battery: number;
+  value: number;
+}
+interface FrameTempSensor {
+  type: 'temperature';
+  battery: number;
+  value: number;
+}
+interface FrameTVOCSensor {
+  type: 'tvoc';
+  battery: number;
+  value: number;
+}
+interface FrameLineBeacon {
+  type: 'line';
+  hwid: string;
+  tx: number;
+  authentication: string;
+  timestamp: number;
+}
+interface FrameDeviceInfo {
+  type: 'info';
+  mac: string;
+  name: string;
+  battery: number;
+}
+export declare type Frame = FrameiBeacon | FrameUID | FrameAccSensor | FrameHTSensor | FrameTLM | FrameURL | FrameLightSensor | FrameForceSensor | FramePIRSensor | FrameTempSensor | FrameTVOCSensor | FrameLineBeacon | FrameDeviceInfo;
+export interface BeaconData {
+  mac: string;
+  name: string;
+  battery: number;
+  rssi: number;
+  lastUpdate: number;
+  frames: Frame[];
+}
